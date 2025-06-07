5d8b606b36e1d6e4b2387f7a5598a67a4be2e7dc
Leandro Fernandez <leanf94@gmail.com>
2025-06-07 15:16:12 -0300

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import './globals.css';
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Filter E-commerce",
@@ -11,7 +12,15 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+            <header>
+                <nav className="bg-amber-500 p-10 flex gap-5">
+                    <Link href="/" className="text-xl font-bold">Home</Link>
+                    <Link href="/products" className="text-xl font-bold">Products</Link>
+                </nav>
+            </header>
+                {children}
+            </body>
 		</html>
 	);
 }
